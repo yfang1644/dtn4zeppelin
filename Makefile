45ef4769bb954d5fdbe4c1df0e1f702a6ce65474
yfang1644 <yfang1644@gmail.com>
2017-08-15 22:59:35 +0800

    add bpchat

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-SRCS 	= bpreceiver.c bpsender.c
+SRCS 	= bpreceiver.c bpsender.c bpchat.c
 CC 		= gcc
 
 EXECS   = ${SRCS:%.c=%}
@@ -6,7 +6,7 @@ EXECS   = ${SRCS:%.c=%}
 all: $(EXECS)
 
 %: %.c
-	$(CC) -o $@ $< -lici -lbp
+	$(CC) -o $@ $< -lici -lbp -pthread
 
 clean:
 	$(RM) -f $(EXECS)
--- bpsender.c
+++ bpsender.c
@@ -24,27 +24,40 @@
 #include <bp.h>
 #define  MAXLINE    1024
 
-int run_bpsendfile(char *ownEid, char *destEid, int ttl, char *svcClass)
+typedef struct
 {
+    BpSAP    sap;
+    int    running;
+} BptestState;
 
-    return 0;
+BptestState *_bptestState(BptestState *newState)
+{
+    void    *value;
+    BptestState    *state;
+
+    if (newState) {           /*    Add task variable.    */
+        value = (void *) (newState);
+        state = (BptestState *) sm_TaskVar(&value);
+    } else {              /*    Retrieve task variable.    */
+        state = (BptestState *) sm_TaskVar(NULL);
+    }
+
+    return state;
 }
 
 int  main(int argc, char **argv)
 {
+    BptestState    state = { NULL, 1  };
     char    *ownEid = NULL;
     char    *destEid = NULL;
     char    *fileName = NULL;
     char    *classOfService = NULL;
     int    ttl = 300;    /*  Time To Live in seconds */
 
-    int  priority = 0;
-    BpAncillaryData    ancillaryData = { 0, 0, 0 };
+    int  priority = BP_STD_PRIORITY;
     BpCustodySwitch    custodySwitch = NoCustodyRequested;
-    BpSAP   sap;
     Sdr     sdr;
     Object  txExtent;
-    int     aduLength;
     Object  bundleZco;
     Object  newBundle;
     char buf[MAXLINE];
@@ -60,53 +73,53 @@ int  main(int argc, char **argv)
         return 0;
     }
 
-    if (classOfService == NULL) {
-        priority = BP_STD_PRIORITY;
-    } else {
-        if (!bp_parse_class_of_service(classOfService, &ancillaryData,
-                &custodySwitch, &priority)) {
-            printf("Invalid class of service for bpsendfile.(%s)\n",
-                    classOfService);
-            return 0;
-        }
-    }
-
     if (bp_attach() < 0) {
         perror("Can't attach to BP.\n");
         return 0;
     }
 
-    if (bp_open(ownEid, &sap) < 0) {
+    if (bp_open(ownEid, &state.sap) < 0) {
         printf("Can't open own endpoint. (%s)\n", ownEid);
         return 0;
     }
+
+    oK(_bptestState(&state));
 
     sdr = bp_get_sdr();
     oK(sdr_begin_xn(sdr));
     if (sdr_heap_depleted(sdr)) {
         sdr_exit_xn(sdr);
-        bp_close(sap);
+        bp_close(state.sap);
         perror("Low on heap space, can't send data.\n");
         return 0;
     }
 
-    txExtent = sdr_malloc(sdr, sizeof(buf));
+    while(state.running) {
+        scanf("%s", buf);
+        txExtent = sdr_malloc(sdr, strlen(buf));
+        sdr_write(sdr, txExtent, buf, strlen(buf));
 
-    sdr_write(sdr, txExtent, buf, strlen(buf) - 1);
-
-    bundleZco = ionCreateZco(ZcoSdrSource, txExtent, 0, sizeof(buf)-1,
-            priority, ancillaryData.ordinal, ZcoOutbound, NULL);
-    if (bundleZco == 0 || bundleZco == (Object) ERROR) {
-        printf("bpsender can't create ZCO.\n");
-    } else {
-        if (bp_send(sap, destEid, NULL, ttl, priority, custodySwitch,
-            0, 0, &ancillaryData, bundleZco, &newBundle) <= 0)
-        {
-            printf("bpsender  can't send data in bundle.\n");
+        bundleZco = ionCreateZco(ZcoSdrSource,
+                                 txExtent,
+                                 0,
+                                 strlen(buf),
+                                 priority,
+                                 0,
+                                 ZcoOutbound,
+                                 NULL);
+        if (bundleZco == 0 || bundleZco == (Object) ERROR) {
+            printf("bpsender can't create ZCO.\n");
+            break;
         }
+        if (bp_send(state.sap, destEid, NULL, ttl, priority, custodySwitch,
+                    0, 0, NULL, bundleZco, &newBundle) <= 0) {
+                        printf("bpsender can't send data in bundle.\n");
+        }
+        break;
     }
 
-    bp_close(sap);
+    bp_close(state.sap);
+    CHKZERO(sdr_begin_xn(sdr));
 
     if (sdr_end_xn(sdr) < 0) {
         return -1;
